99a134eb969e2ffc72cff39c18f9262dd0ebbf30
queonias <queones@gmail.com>
2024-04-22 19:38:15 -0300

    temporizador e textfild de tempo criado

--- lib/telas/timer.dart
+++ lib/telas/timer.dart
@@ -26,6 +26,7 @@ class Timer extends StatefulWidget {
 
 class _TimerState extends State<Timer> {
   final CountDownController _clockController = CountDownController();
+  final TextEditingController _controllerTempo = TextEditingController();
   Icon _clockButton = kPlayClockButton; // Initial value
   bool _isClockStarted = false; // Conditional flag
   int indexTimesCompleted = 0; // Defina a variável aqui
@@ -50,39 +51,13 @@ class _TimerState extends State<Timer> {
     }
   }
 
-  // void handleTimerCompletion() {
-  //   setState(() async {
-  //     widget.timesCompleted[indexTimesCompleted] = const Icon(
-  //       Icons.brightness_1_rounded,
-  //       color: Colors.pink,
-  //       size: 5.0,
-  //     );
-  //     indexTimesCompleted++;
-  //     await NDialog(
-  //       dialogStyle: DialogStyle(titleDivider: true),
-  //       title: const Text("Timer Completed"),
-  //       content: const Text("Time to break."),
-  //       actions: <Widget>[
-  //         ElevatedButton(
-  //             style: ButtonStyle(
-  //               backgroundColor:
-  //                   MaterialStateColor.resolveWith((states) => Colors.green),
-  //             ),
-  //             child: const Text("Start a short break"),
-  //             onPressed: () {}),
-  //       ],
-  //     ).show(context);
-  //   });
-  // }
-
   void handleTimerCompletion() async {
     // Execute a lógica assíncrona fora do setState
-    await Future.delayed(Duration
-        .zero); // Adicione um pequeno atraso para garantir que a execução seja assíncrona
+    await Future.delayed(Duration.zero);
 
     // Atualize o estado de forma síncrona dentro do setState
     setState(() {
-      widget.timesCompleted[indexTimesCompleted] = Icon(
+      widget.timesCompleted[indexTimesCompleted] = const Icon(
         Icons.brightness_1_rounded,
         color: Colors.pink,
         size: 5.0,
@@ -93,8 +68,8 @@ class _TimerState extends State<Timer> {
     // Mostrar o diálogo
     await NDialog(
       dialogStyle: DialogStyle(titleDivider: true),
-      title: Text("Timpo Completado"),
-      content: Text("Hora de fazer uma pausa."),
+      title: const Text("Tempo Completado"),
+      content: const Text("Hora de fazer uma pausa."),
       actions: <Widget>[
         ElevatedButton(
           style: ButtonStyle(
@@ -102,7 +77,7 @@ class _TimerState extends State<Timer> {
               (states) => const Color.fromARGB(255, 99, 179, 245),
             ),
           ),
-          child: Text("Iniciar uma pausa curta"),
+          child: const Text("Iniciar uma pausa curta"),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -112,14 +87,37 @@ class _TimerState extends State<Timer> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    // Adicione um listener ao TextEditingController para ouvir as mudanças de texto
+    _controllerTempo.addListener(() {
+      setState(() {
+        // Obtenha o valor do texto do TextEditingController
+        String tempoTexto = _controllerTempo.text;
+        if (_controllerTempo.text.isEmpty) {
+          tempoTexto = '1';
+        }
+        // Verifique se o texto é um número válido
+        int? tempo = int.tryParse(tempoTexto);
+        // Atualize o tempo do relógio com base no novo valor inserido ou defina um valor padrão
+        if (tempo == null || tempo <= 0) {
+          tempo = 1;
+        }
+        _clockController.restart(duration: tempo * 60);
+      });
+      _clockButton = kPlayClockButton;
+      _clockController.pause();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Half Screen Dimensions
     final double height = MediaQuery.of(context).size.height / 2;
     final double width = MediaQuery.of(context).size.width / 2;
-    int indexTimesCompleted = 0;
 
     CountDownTimer _countDownTimer = CountDownTimer(
-      duration: kWorkDuration,
+      duration: int.parse(
+          _controllerTempo.text.isEmpty ? '60' : _controllerTempo.text),
       fillColor: Colors.pink,
       onComplete: handleTimerCompletion,
     );
@@ -131,7 +129,7 @@ class _TimerState extends State<Timer> {
       height: height,
       width: width,
       autoStart: false,
-      duration: _countDownTimer.duration * 60,
+      duration: _countDownTimer.duration,
       isReverse: true,
       textStyle: const TextStyle(color: Colors.white),
       fillColor: _countDownTimer.fillColor,
@@ -156,47 +154,59 @@ class _TimerState extends State<Timer> {
           ),
         ],
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: clock,
-              ),
-              const Text(
-                kWorkLabel,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: widget.timesCompleted,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      switchClockActionButton();
-                    });
-                  },
-                  child: Container(
-                    width: width / 2.5,
-                    height: height / 8,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(10.0),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: TextField(
+                    controller: _controllerTempo,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: 'Tempo',
+                      border: OutlineInputBorder(),
                     ),
-                    child: _clockButton,
                   ),
                 ),
-              ),
-            ],
+                Center(
+                  child: clock,
+                ),
+                const Text(
+                  kWorkLabel,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: widget.timesCompleted,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        switchClockActionButton();
+                      });
+                    },
+                    child: Container(
+                      width: width / 2.5,
+                      height: height / 8,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: _clockButton,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
